cfbeb27523ba6f9921ef7b9898fc786da179d720
maisona telier <maisonatelier52@gmail.com>
2026-02-25 11:26:18 +0530

    new content added feb 25

--- src/components/OrinoxPage.tsx
+++ src/components/OrinoxPage.tsx
@@ -25,11 +25,7 @@ export default function OrinoxPage({data}:OrinoxPageProps) {
       <div className="absolute inset-0 bg-black/50" />
 
       <div className="absolute top-8 right-10 md:right-16 z-20 flex items-center gap-3">
-        <img
-          src="/images/orinox-white-logo.png"
-          alt="Orinox Logo"
-          className="h-10 md:h-12 w-auto"
-        />
+        
       </div>
 
    <div className="absolute bottom-5 -translate-y-1/2 left-10 sm:left-10 lg:left-25 z-20 space-y-30 max-w-xs sm:max-w-md lg:max-w-xl text-white flex flex-col">        <div>
