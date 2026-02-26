2837d29004cb542a4486b716609b8c9ba518b55b
maisona telier <maisonatelier52@gmail.com>
2026-02-26 16:43:46 +0530

    alignment fixed01

--- src/components/Orinox1.tsx
+++ src/components/Orinox1.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Orinox1() {
   return (
-    <section className="min-h-screen w-full bg-black px-6 py-10 md:px-20 md:pt-20 text-white">
-      <div className="max-w-[1100px] ">
+    <section className="h-screen w-full bg-black py-12 px-8 md:px-16 lg:px-24 md:pt-20 text-white overflow-hidden">
+      <div className="max-w-[1100px] h-full mx-auto flex flex-col justify-between">
         {/* Top bar */}
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-15 md:gap-0">
           <img
@@ -23,9 +23,9 @@ export default function Orinox1() {
 
         {/* Menu */}
         {/* Menu */}
-        <div className="mt-20 md:mt-40 text-[12px]">
+        <div className="md:mt-40 text-[12px]">
           <div className="mx-auto max-w-[1100px]">
-            <div className="grid grid-cols-2 md:grid-cols-5 gap-y-10 gap-x-6 md:gap-x-12">
+            <div className="grid grid-cols-2 md:grid-cols-5 gap-y-6 md:gap-y-10 gap-x-6 md:gap-x-12">
               <div className="flex flex-col gap-4">
                 <p>About</p>
                 <p>Services</p>
@@ -60,7 +60,7 @@ export default function Orinox1() {
           </div>
         </div>
         {/* ACTION BUTTONS */}
-        <div className="mt-10 md:mt-16 flex flex-col sm:flex-row justify-end gap-4">
+        <div className="md:mt-16 flex flex-col sm:flex-row justify-end gap-4 mb-4 md:mb-0">
           <a
             href="/pdf/orinox.pdf"
             className="text-center rounded-md border border-white/60 px-6 py-2 text-[12px] font-light hover:border-white transition"
